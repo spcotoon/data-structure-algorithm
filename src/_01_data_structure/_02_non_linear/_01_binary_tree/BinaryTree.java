a777e223c0edef88fac28720b7afcbb93fb769ba
spcotoon <springcomicskr@gmail.com>
2025-07-26 18:47:16 +0900

    add remove function

--- src/_01_data_structure/_02_non_linear/_01_binary_tree/BinaryTree.java
+++ src/_01_data_structure/_02_non_linear/_01_binary_tree/BinaryTree.java
@@ -84,4 +84,16 @@ public class BinaryTree<T> {
         postOrderTraversal(tree.getRightSubTree(), result);
         result.add(tree.getData());
     }
+
+    private BinaryTree<T> removeLeftSubTree() {
+        BinaryTree<T> deletingNode = this.getLeftSubTree();
+        this.setLeftSubTree(null);
+        return deletingNode;
+    }
+
+    private BinaryTree<T> removeRightSubTree() {
+        BinaryTree<T> deletingNode = this.getRightSubTree();
+        this.setRightSubTree(null);
+        return deletingNode;
+    }
 }
